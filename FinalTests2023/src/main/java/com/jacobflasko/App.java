03c2907dfc7a2c1cea7bb6767af60bf59f070853
Jacob Flasko <jrf21@students.ptcollege.edu>
2023-12-15 13:33:34 -0500

    Reworked Index
    Styled, and added images for visual representation on index.cshtml.

--- FinalTests2023/src/main/java/com/jacobflasko/App.java
+++ FinalTests2023/src/main/java/com/jacobflasko/App.java
@@ -4,10 +4,16 @@ package com.jacobflasko;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        Thread.sleep(10_000);
+        System.out.println("Hello World!");
+
+        for (int j = 1; j <= 10; j++) {
+            Thread.sleep(1_000);
+            System.out.println(j);
+        }
+
     }
 }
